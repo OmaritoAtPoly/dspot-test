660a2cc04b50134ef18b11574fd53e4987656ea5
Omar Estrada Diaz <omarito4857@gmail.com>
2021-10-18 02:07:20 -0400

    solving cards problem added in cardPackages fn

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import cardPackages from './containers/cardPackages';
+import givenCards from './utils/givenCards';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <p>
+      react test
+    </p>
+  </div>
+)
 
 export default App;
---
+++ src/containers/cardPackages.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import givenCards from "../utils/givenCards";
+
+type Cards = {
+  suit: string;
+  value: string | number;
+};
+
+const cardPackages = (cards: Cards[]) => {
+  const cardSet = new Map();
+
+  cards.forEach((card) => {
+    const cardName = `${card.suit} ${card.value}`;
+    const cardElement = cardSet.get(cardName) || 0;
+    cardSet.set(cardName, cardElement + 1);
+  });
+
+  const values = cardSet.values() as unknown as number[];
+  if (cardSet.size < 52) return 0;
+  return [...values].sort((a, b) => a - b)[0];
+};
+
+export default cardPackages;
